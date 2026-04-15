8772107c252704c30d2a9980bc312b9eb3629db0
joshua <joshgabethomas@gmail.com>
2026-04-15 10:15:27 +0800

    Feat/auth: Updated dtos to take in a file, updated the application properties to contain vars needed for the avatar service

--- services/auth/src/main/java/org/acme/dto/UserSummaryDTO.java
+++ services/auth/src/main/java/org/acme/dto/UserSummaryDTO.java
@@ -1,11 +1,15 @@
 package org.acme.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class UserSummaryDTO {
     // Basic information about the user, for dropdown menus and other places where we don't need all the details
     public Long id;
     public String username;
     public String fullName;
-    public String avatarUrl;
+    public String avatarImage;
+    @JsonIgnore
+    public String avatarPath;
 
     public UserSummaryDTO() {}
 
@@ -13,6 +17,6 @@ public class UserSummaryDTO {
         this.id = id;
         this.username = username;
         this.fullName = fullName;
-        this.avatarUrl = avatarUrl;
+        this.avatarPath = avatarUrl;
     }
 }
